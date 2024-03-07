e13429ab8f1729362934518ca97e548f4c437ef8
Jackson Barreto <jackson.barreto@gmail.com>
2024-03-07 09:54:24 +0000

    scanner

--- Dockerfile
+++ Dockerfile
@@ -17,15 +17,38 @@ WORKDIR /app/cmd/stls
 # Build the application with optimization flags
 RUN go build -ldflags="-w -s" -o app ./main.go
 
+
+
 # Stage 2: running the application
 FROM alpine:3.19
 
 # Add a non-root user
 RUN adduser -D stls
-USER stls
+
+# Install OpenSSL and bash for testssl.sh, and any other dependencies
+RUN apk add --no-cache bash openssl coreutils procps
+
+# Download testssl.sh
+WORKDIR /home/stls
+RUN wget https://github.com/drwetter/testssl.sh/archive/refs/heads/3.0.zip && \
+    unzip 3.0.zip && \
+    mv testssl.sh-3.0 testssl.sh && \
+    rm 3.0.zip && \
+    chmod +x testssl.sh/testssl.sh && \
+    mkdir "results" && \
+    chmod 700 "results"
+
 
 # Copy the compiled application from the builder stage
-COPY --from=builder /app/cmd/stls/app /stls/app
+COPY --from=builder /app/cmd/stls/app /home/stls/app
+
+# Change ownership to the non-root user
+RUN chown -R stls:stls /home/stls
+
+USER stls
+
+# Set the volume where the results will be stored
+VOLUME ["/home/stls/results"]
 
 # Run the compiled binary
-CMD ["/stls/app"]
+CMD ["/home/stls/app"]
---
+++ internal/consumer/consumer_group_handler.go
@@ -0,0 +1,49 @@
+package consumer
+
+import (
+	"github.com/IBM/sarama"
+	"github.com/jacksonbarreto/DNSSECAnalyzer/pkg/logservice"
+	"github.com/jacksonbarreto/stls/config"
+	"github.com/jacksonbarreto/stls/scanner"
+)
+
+type Scanner interface {
+}
+
+type GroupHandler struct {
+	scanner *scanner.Scanner
+	Log     logservice.Logger
+}
+
+func NewConsumerGroupHandler(scanner *scanner.Scanner, logService logservice.Logger) *GroupHandler {
+	return &GroupHandler{
+		scanner: scanner,
+		Log:     logService,
+	}
+}
+
+func NewConsumerGroupHandlerDefault(scanner *scanner.Scanner) *GroupHandler {
+	return NewConsumerGroupHandler(scanner, logservice.NewLogService(config.App().Id))
+}
+
+func (h *GroupHandler) Setup(session sarama.ConsumerGroupSession) error {
+	return nil
+}
+
+func (h *GroupHandler) Cleanup(session sarama.ConsumerGroupSession) error {
+	return nil
+}
+
+func (h *GroupHandler) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+	for message := range claim.Messages() {
+		h.Log.Info("Message claimed: value = %s, timestamp = %v, topic = %s", string(message.Value), message.Timestamp, message.Topic)
+		err := h.scanner.Scan(string(message.Value))
+		if err != nil {
+			h.Log.Error("Error scanning host '%s': %v", string(message.Value), err)
+			continue
+		}
+		h.Log.Info("Message processed: value = %s, timestamp = %v, topic = %s", string(message.Value), message.Timestamp, message.Topic)
+		session.MarkMessage(message, "")
+	}
+	return nil
+}
